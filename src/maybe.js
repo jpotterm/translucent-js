5637b81f6a2b3c938f96f286a237d33731e09325
Jonathan Potter <jpotterm@gmail.com>
2014-06-09 11:11:35 -0400

    Add more functions to work with maybes more easily

--- src/maybe.js
+++ src/maybe.js
@@ -8,8 +8,24 @@ tlc.Maybe = function(hasValue, value) {
     this.value = value;
 };
 
+tlc.isJust = function(maybe) {
+    return maybe.hasValue;
+};
+
+tlc.isNothing = function(maybe) {
+    return !maybe.hasValue;
+};
+
+tlc.maybe = function(defaultValue, f, maybe) {
+    return tlc.fromMaybe(defaultValue, tlc.map(f, maybe));
+};
+
+tlc.fromMaybe = function(defaultValue, maybe) {
+    return maybe.hasValue ? maybe.value : defaultValue;
+};
+
 tlc.catMaybes = function(xs) {
-    var justs = tlc.filter(tlc.prop("hasValue"), xs);
+    var justs = tlc.filter(tlc.isJust, xs);
     return tlc.map(tlc.prop("value"), justs);
 };
 
--- test/maybe.js
+++ test/maybe.js
@@ -3,6 +3,48 @@
 var tlc = require("../src/index.js");
 
 
+describe("maybe.isJust", function() {
+    it("should tell if a Maybe is a Just", function() {
+        var just = new tlc.Maybe(true, 1);
+        var nothing = new tlc.Maybe(false);
+
+        expect(tlc.isJust(just)).toBe(true);
+        expect(tlc.isJust(nothing)).toBe(false);
+    });
+});
+
+describe("maybe.isNothing", function() {
+    it("should tell if a Maybe is a Nothing", function() {
+        var just = new tlc.Maybe(true, 1);
+        var nothing = new tlc.Maybe(false);
+
+        expect(tlc.isNothing(just)).toBe(false);
+        expect(tlc.isNothing(nothing)).toBe(true);
+    });
+});
+
+describe("maybe.maybe", function() {
+    it("should return the result of f or the default value", function() {
+        function plusOne(x) {return x + 1;}
+
+        var just = new tlc.Maybe(true, 2);
+        var nothing = new tlc.Maybe(false);
+
+        expect(tlc.maybe(1, plusOne, just)).toBe(3);
+        expect(tlc.maybe(1, plusOne, nothing)).toBe(1);
+    });
+});
+
+describe("maybe.fromMaybe", function() {
+    it("should give a default value if Maybe is Nothing", function() {
+        var just = new tlc.Maybe(true, 2);
+        var nothing = new tlc.Maybe(false);
+
+        expect(tlc.fromMaybe(1, just)).toBe(2);
+        expect(tlc.fromMaybe(1, nothing)).toBe(1);
+    });
+});
+
 describe("maybe.catMaybes", function() {
     it("should filter out Nothings and unwrap values", function() {
         var xs = [
